4832249f85d8c96889fa0b900ded78f70b76e8ef
Simon Bennett <simon@bennett.im>
2015-10-27 20:37:45 +0000

    Replace Quotes
    /Users/simon/fullrent/react/bin/webpack-dev-server.js:9
      console.log(`Webpack dev server running at ${host}: ${port}`);
                  ^
    SyntaxError: Unexpected token ILLEGAL

--- bin/webpack-dev-server.js
+++ bin/webpack-dev-server.js
@@ -6,5 +6,5 @@ const devServer = require('../build/webpack-dev-server'),
 const host = config.get('webpack_host');
 const port = config.get('webpack_port');
 devServer.listen(port, host, function () {
-  console.log(`Webpack dev server running at ${host}: ${port}`);
+  console.log('Webpack dev server running at ${host}: ${port}');
 });
